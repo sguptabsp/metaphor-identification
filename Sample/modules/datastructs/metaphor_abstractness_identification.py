5d12ec5ac41a2ec07dd0bfaa89c0de3948bf9160
sguptabsp <sgupta45@hawk.iit.edu>
2019-09-09 22:42:58 -0500

    Updated file paths to be generic and os independent

--- Sample/modules/datastructs/metaphor_abstractness_identification.py
+++ Sample/modules/datastructs/metaphor_abstractness_identification.py
@@ -2,6 +2,7 @@ from Sample.modules.datastructs.metaphor_identification import MetaphorIdentific
 from Sample.modules.datastructs.annotated_text import AnnotatedText
 import pandas as pd
 
+
 class MetaphorAbstractnessIdentification(MetaphorIdentification):
     def __init__(self, text=""):
         self.rawText = text
@@ -15,13 +16,20 @@ class MetaphorAbstractnessIdentification(MetaphorIdentification):
 
     def findCandidates(self, identificationFunction):
         fields = ['adj', 'noun']
-        MET_AN_EN_TEST = pd.read_excel('/home/shrutitejus/iit/research_project/Research_Project/Sample/modules/datastructs/Datasets_ACL2014.xlsx', sheetname='MET_AN_EN', usecols=fields)
+        MET_AN_EN_TEST = pd.read_excel(
+            '../data/Datasets_ACL2014.xlsx',
+            sheetname='MET_AN_EN', usecols=fields)
         MET_AN_EN_TEST['class'] = 1
-        LIT_AN_EN_TEST = pd.read_excel('/home/shrutitejus/iit/research_project/Research_Project/Sample/modules/datastructs/Datasets_ACL2014.xlsx', sheetname='LIT_AN_EN', usecols=fields)
+        LIT_AN_EN_TEST = pd.read_excel(
+            '../data/Datasets_ACL2014.xlsx',
+            sheetname='LIT_AN_EN', usecols=fields)
         LIT_AN_EN_TEST['class'] = 0
-        MET_AN_EN = pd.read_table('/home/shrutitejus/iit/research_project/Research_Project/Sample/modules/datastructs/training_adj_noun_met_en.txt', delim_whitespace=True, names=('adj', 'noun'))
+        MET_AN_EN = pd.read_table(
+            '../data/training_adj_noun_met_en.txt',
+            delim_whitespace=True, names=('adj', 'noun'))
         MET_AN_EN['class'] = 1
-        LIT_AN_EN = pd.read_table('/home/shrutitejus/iit/research_project/Research_Project/Sample/modules/datastructs/training_adj_noun_nonmet_en.txt', delim_whitespace=True, names=('adj', 'noun'))
+        LIT_AN_EN = pd.read_table('../data/training_adj_noun_nonmet_en.txt', delim_whitespace=True,
+                                  names=('adj', 'noun'))
         LIT_AN_EN['class'] = 0
 
         df = pd.concat([LIT_AN_EN_TEST, MET_AN_EN_TEST, LIT_AN_EN, MET_AN_EN])
